b17c5ec57f6adb4c267cf31e7e1970c4e97aee2d
theSopan21 <sopan@snoodify.com>
2024-12-29 01:26:01 +0530

    another way to avoid any error

--- src/utils/processEnvVaribale.ts
+++ src/utils/processEnvVaribale.ts
@@ -2,6 +2,20 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+// another way to export environment varibale
+/**
+  interface EnvironmentVarible {
+    env:{
+      [key:string] : string
+    }
+  }
+
+  const { env } = process as EnvironmentVarible;
+
+  export const MONGO_DB_URI = env.DB_URI;
+  export const PORT = env.PORT;
+*/
+
 const processEnvironmentVariable = () => {
   return process.env;
 };
