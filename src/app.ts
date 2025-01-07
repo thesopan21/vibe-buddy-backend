f793748d4789b16e0c7fd9e49866131d497b8c08
theSopan21 <sopan@snoodify.com>
2025-01-07 23:59:34 +0530

    dummy

--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,7 @@ app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static('src/public'))
 
 // user routes
 app.use("/api/v1/user", userRoutes);
