e3fc10478405ac4ba6f911ac3b74a7ab251fbae3
theSopan21 <sopan@snoodify.com>
2025-01-11 17:42:18 +0530

    add cloudinary config

---
+++ src/config/cloudinary.ts
@@ -0,0 +1,11 @@
+import { CLOUDINARY_API_KEY, CLOUDINARY_CLOUD_NAME, CLOUDINARY_SECRET_KEY } from "@/utils/processEnvVaribale";
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: CLOUDINARY_CLOUD_NAME,
+  api_key: CLOUDINARY_API_KEY,
+  api_secret: CLOUDINARY_SECRET_KEY,
+});
+
+
+export default cloudinary;
