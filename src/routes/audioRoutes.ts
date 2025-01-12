b661524888524f06f13582f294c13dd893871ae4
theSopan21 <sopan@snoodify.com>
2025-01-12 17:07:39 +0530

    verify user middleware added

--- src/routes/audioRoutes.ts
+++ src/routes/audioRoutes.ts
@@ -1,5 +1,6 @@
 import { addNewAudioController } from "@/controllers/audioController";
 import { isAuthUserController } from "@/controllers/userController";
+import { isVerifiedUserMiddleware } from "@/middlewares/authMiddleware";
 import { parseFileMiddleware } from "@/middlewares/fileParserMiddleware";
 import { schemaValidatorMiddleware } from "@/middlewares/schemaValidationMiddleware";
 import { AudioSchemaValidations } from "@/validations/audioModelValidations";
@@ -10,7 +11,7 @@ const audioRoutes = Router();
 audioRoutes.post(
   "/add-new-audio",
   isAuthUserController,
-  
+  isVerifiedUserMiddleware,
   parseFileMiddleware,
   schemaValidatorMiddleware(AudioSchemaValidations),
   addNewAudioController
