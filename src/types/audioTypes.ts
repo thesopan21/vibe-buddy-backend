f8d822e2f2f360328994ee0173a63ae14b346864
theSopan21 <sopan@snoodify.com>
2025-01-12 14:50:48 +0530

    add types for audio schema

---
+++ src/types/audioTypes.ts
@@ -0,0 +1,18 @@
+import { AudioCategoryTypes } from "@/utils/audioCategories";
+import { ObjectId } from "mongoose";
+
+export interface AudioDocumentSchema {
+  title: string;
+  about: string;
+  owner: ObjectId;
+  file: {
+    url: string;
+    publicId: string;
+  };
+  poster: {
+    url: string;
+    publicId: string;
+  };
+  likes: ObjectId[];
+  categgories: AudioCategoryTypes;
+}
---
+++ src/utils/audioCategories.ts
@@ -0,0 +1,22 @@
+export const categories = [
+  "Arts",
+  "Bussiness",
+  "Educations",
+  "Entertainment",
+  "Kids & Family",
+  "Music",
+  "Science",
+  "Tech",
+  "Other",
+];
+
+export type AudioCategoryTypes =
+  | "Arts"
+  | "Bussiness"
+  | "Educations"
+  | "Entertainment"
+  | "Kids & Family"
+  | "Music"
+  | "Science"
+  | "Tech"
+  | "Other";
