8a91b64cd89d85a3563753215f90d571f54aece9
theSopan21 <sopan@snoodify.com>
2025-01-21 00:00:28 +0530

    add playlist validation

---
+++ src/validations/playlistValidation.ts
@@ -0,0 +1,22 @@
+import { isValidObjectId } from "mongoose";
+import { object, string } from "yup";
+
+const titleValidation = string()
+  .required("Title is required!")
+  .trim()
+  .min(4, "title length must be greater than 4 character")
+  .max(32, "Title length must be less than 32 character");
+
+const audioIdValidation = string().transform(function (value) {
+  return this.isType(value) && isValidObjectId(value) ? value : "";
+});
+
+const visibilityValidation = string()
+  .oneOf(["public", "private"], "invalid category")
+  .required("Visibility is missing!");
+
+export const newPlayListValidatonSchema = object().shape({
+  title: titleValidation,
+  audioId: audioIdValidation,
+  visibility: visibilityValidation,
+});
